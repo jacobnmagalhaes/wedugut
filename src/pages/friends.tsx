695ada5197c5bd14c1283b039ba71a4835f005e0
harish <chandharish198@gmaiul.com>
2023-02-14 22:20:39 +0530

    engagemnet image updated

--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 export default function Friends() {
 
 
-    return ( 
-        <div className='bg-gradient-to-t from-white to-[#3399cc] h-screen ' >
+    return (
+        <div className='bg-gradient-to-t from-white to-[#3399cc] min-h-screen max-h-screen overflow-y-hidden ' >
 
 
-            <div className='flex justify-center  h-screen overflow-y-hidden pt-[258px] md:pt-8 '>
+            <div className='flex justify-center items-end h-screen  '>
 
                 <img src="images/startup/friends.webp" className='object-contain' alt="" />
             </div>
-            <div className='h-[18vh] flex flex-col justify-center absolute bottom-5 md:bottom-6 w-full  '>
-               
+            <div className='h-[18vh] flex flex-col justify-center absolute bottom-0 md:bottom-6 w-full  '>
+
                 <a href='https://friends.dugut.app' target={'blank'} className='-mb-8 md:-mb-20'>
                     <img src="images/startup/GOButton.webp" className='cursor-pointer hover:brightness-110 w-40 m-auto ' alt="" />
                 </a>
