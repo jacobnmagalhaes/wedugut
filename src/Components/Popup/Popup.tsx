98f12cde96aef59c7c237919107ccccedc784bfb
YegorKorenkov <eg.korenkov@gmai.com>
2023-02-27 15:30:09 +0200

    Add separate popup component

--- src/Components/Popup/Popup.tsx
+++ src/Components/Popup/Popup.tsx
@@ -1,8 +1,11 @@
-import Image from 'next/image'
-import { FC, useRef, useState } from 'react'
+import { FC, ReactNode, useRef, useState } from 'react'
 import useOutsideClick from '../../hooks/useOutsideClick'
 
-const Popup: FC = () => {
+interface IProps {
+  children: ReactNode
+}
+
+const Popup: FC<IProps> = ({ children }) => {
   const [isVisible, setIsVisible] = useState(true)
   const ref = useRef(null)
 
@@ -16,57 +19,7 @@ const Popup: FC = () => {
           <div
             ref={ref}
             className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 popup max-w-2xl z-30 flex flex-col items-center justify-center text-white'>
-            <img src='/images/main/MAIN PAGE POPUP.png' />
-            <p className='font-bold mb-4 text-sm md:text-base'>
-              Help our volunteers to create a better future for all of us! All our content is
-              created by people like you that want to become part of the solution!
-            </p>
-            <p className='text-sm md:text-base'>
-              Go on a journey and learn about our most pressing issues and share your knowledge with
-              your friends and family.{' '}
-              <b>By engaging in a journey, you can take real-life actions</b> that are performed by
-              our fast-growing army of volunteers. You need 500 points to redeem your Action Reward!
-              Journeys with a star are the most active journeys.
-              <b>
-                {' '}
-                Watch videos of our actions, we have already taken over 20.000 of them and we just
-                started!
-              </b>
-            </p>
-
-            <h3 className='uppercase text-center text-sm mt-9 mb-6'>Choose your action</h3>
-            <div className='flex items-center relative gap-12 md:px-12'>
-              <img className='w-24 lg:w-32' src='/images/main/Journey.png' />
-              <div className='flex flex-col w-3/5'>
-                <p className='text-sm mb-2'>
-                  Go on an action journey to learn about sustainable issues and solutions and take a
-                  real-live action! (+15 Minutes)
-                </p>
-                <img src='/images/main/Gobutton-1.webp' className='w-32 cursor-pointer' />
-              </div>
-            </div>
-
-            <div className='flex items-center relative gap-12 md:px-12'>
-              <img className='w-24 lg:w-32' src='/images/main/invite.png' />
-              <div className='flex flex-col w-3/5'>
-                <p className='text-sm mb-2'>
-                  Go on an action journey to learn about sustainable issues and solutions and take a
-                  real-live action! (+15 Minutes)
-                </p>
-                <img src='/images/main/Gobutton-1.webp' className='w-32 cursor-pointer' />
-              </div>
-            </div>
-
-            <div className='flex items-center relative gap-12 md:px-12'>
-              <img className='w-24 lg:w-32' src='/images/main/Newsletter.png' />
-              <div className='flex flex-col w-3/5'>
-                <p className='text-sm mb-2'>
-                  Go on an action journey to learn about sustainable issues and solutions and take a
-                  real-live action! (+15 Minutes)
-                </p>
-                <img src='/images/main/Gobutton-1.webp' className='w-32 cursor-pointer' />
-              </div>
-            </div>
+            {children}
           </div>
         </div>
       )}
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -16,7 +16,60 @@ export default function HomePage() {
   }
   return (
     <>
-      <Popup />
+      <Popup>
+        <>
+          <img src='/images/main/MAIN PAGE POPUP.png' />
+          <p className='font-bold mb-4 text-sm md:text-base'>
+            Help our volunteers to create a better future for all of us! All our content is created
+            by people like you that want to become part of the solution!
+          </p>
+          <p className='text-sm md:text-base'>
+            Go on a journey and learn about our most pressing issues and share your knowledge with
+            your friends and family. <b>By engaging in a journey, you can take real-life actions</b>{' '}
+            that are performed by our fast-growing army of volunteers. You need 500 points to redeem
+            your Action Reward! Journeys with a star are the most active journeys.
+            <b>
+              {' '}
+              Watch videos of our actions, we have already taken over 20.000 of them and we just
+              started!
+            </b>
+          </p>
+
+          <h3 className='uppercase text-center text-sm mt-9 mb-6'>Choose your action</h3>
+          <div className='flex items-center relative gap-12 md:px-12'>
+            <img className='w-24 lg:w-32' src='/images/main/Journey.png' />
+            <div className='flex flex-col w-3/5'>
+              <p className='text-sm mb-2'>
+                Go on an action journey to learn about sustainable issues and solutions and take a
+                real-live action! (+15 Minutes)
+              </p>
+              <img src='/images/main/Gobutton-1.webp' className='w-32 cursor-pointer' />
+            </div>
+          </div>
+
+          <div className='flex items-center relative gap-12 md:px-12'>
+            <img className='w-24 lg:w-32' src='/images/main/invite.png' />
+            <div className='flex flex-col w-3/5'>
+              <p className='text-sm mb-2'>
+                Go on an action journey to learn about sustainable issues and solutions and take a
+                real-live action! (+15 Minutes)
+              </p>
+              <img src='/images/main/Gobutton-1.webp' className='w-32 cursor-pointer' />
+            </div>
+          </div>
+
+          <div className='flex items-center relative gap-12 md:px-12'>
+            <img className='w-24 lg:w-32' src='/images/main/Newsletter.png' />
+            <div className='flex flex-col w-3/5'>
+              <p className='text-sm mb-2'>
+                Go on an action journey to learn about sustainable issues and solutions and take a
+                real-live action! (+15 Minutes)
+              </p>
+              <img src='/images/main/Gobutton-1.webp' className='w-32 cursor-pointer' />
+            </div>
+          </div>
+        </>
+      </Popup>
       <HeaderComponent image={'/images/homePage/Logo.webp'} />{' '}
       <div className='min-h-[100vh] flex'>
         <SideBarMenu />
