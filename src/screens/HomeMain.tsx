d293fe133d57fb6ad4062b93d8c8c68d0ad5b0da
YegorKorenkov <eg.korenkov@gmai.com>
2023-03-01 00:27:44 +0200

    Fix

--- src/screens/HomeMain.tsx
+++ src/screens/HomeMain.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { FC } from 'react'
 import ActionCard from '../Components/ActionCard/ActionCard'
 import ActionSection from '../Components/ActionSection/ActionSection'
