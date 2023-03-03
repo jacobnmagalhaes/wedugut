6bab55cc898162f420906df00cc8c773ba836ac6
YegorKorenkov <eg.korenkov@gmai.com>
2023-03-03 19:55:00 +0200

    Add query param dependency for main popup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,21 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../redux/store'
-
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { push } = useRouter()
+
+  useEffect(() => {
+    push({
+      query: {
+        source: 'dugutapp',
+      },
+    })
+  }, [])
   return (
     <Provider store={store}>
-
       <Component {...pageProps} />
     </Provider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import { FooterComponent } from '../Components/footer'
 import { HeaderComponent } from '../Components/header'
 import SideBarMenu from '../Components/sidebar/SideBarMenu'
@@ -6,7 +7,14 @@ import HomeMain from '../screens/Main/HomeMain'
 import HomeMainPopup from '../screens/Main/HomeMainPopup'
 
 export default function HomePage() {
-  const [isOpen, setIsOpen] = useState(true)
+  const { query } = useRouter()
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    if (query.source == 'dugutapp') {
+      setIsOpen(true)
+    }
+  }, [query])
 
   return (
     <>
