6fc96199eacc3fbd9041e377495efbaf5ebd74d9
YegorKorenkov <eg.korenkov@gmai.com>
2023-03-06 20:16:16 +0200

    Add background image to social/shop pages

--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -6,18 +6,30 @@ export default function Friends() {
   const [isVisible, setIsVisible] = useState(true)
 
   return (
-    <Popup isOpen={isVisible} setIsOpen={setIsVisible} hideOnClickOverlay={false}>
-      <>
-        <div className='md:w-[82%]'>
-          <Image src='/images/friends/FRIENDS PAGE POPUP.png' width={2211} height={3009} />
-        </div>
-        <a href='https://friends.dugut.app/' target='_blank'>
-          <img
-            src='/images/main/Gobutton-1.webp'
-            className='w-44 cursor-pointer hover:brightness-110 duration-100'
-          />
-        </a>
-      </>
-    </Popup>
+    <>
+      <div className='w-[100vw] h-[100vh] hidden md:block'>
+        <Image src={'/images/friends/SOCIAL PC.png'} layout='fill' alt='Social background image' />
+      </div>
+      <div className='w-[100vw] h-[100vh] md:hidden'>
+        <Image
+          src={'/images/friends/SOCIAL MOBILE.png'}
+          layout='fill'
+          alt='Social background image'
+        />
+      </div>
+      <Popup isOpen={isVisible} setIsOpen={setIsVisible} hideOnClickOverlay={false}>
+        <>
+          <div className='md:w-[82%]'>
+            <Image src='/images/friends/FRIENDS PAGE POPUP.png' width={2211} height={3009} />
+          </div>
+          <a href='https://friends.dugut.app/' target='_blank'>
+            <img
+              src='/images/main/Gobutton-1.webp'
+              className='w-44 cursor-pointer hover:brightness-110 duration-100'
+            />
+          </a>
+        </>
+      </Popup>
+    </>
   )
 }
